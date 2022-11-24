af875e39cb436e2af60da2461f1f466e6ac08190
Jurasio54 <103755947+Jurasio54@users.noreply.github.com>
2022-11-23 19:30:40 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 import ProxyCloud
 
-BOT_TOKEN =  os.environ.get('bot_token','5641357578:AAEOn86oQuYSbTmXncg4f8EYjSS0XU7WqJk')
-API_ID =  os.environ.get('api_id','15158057')
-API_HASH = os.environ.get('api_hash','6a7956a4a75bd2ed46a021649e4c9edf')
+BOT_TOKEN =  os.environ.get('bot_token','5495079265:AAHjhcna03KLCpc2qnYON49x2RCzV4Kn_O0')
+API_ID =  os.environ.get('api_id','15472343')
+API_HASH = os.environ.get('api_hash','6817c82fe0b762ed91999f49cd17f7d7')
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
-ACCES_USERS = os.environ.get('tl_admin_user','potterhead5').split(';')
-PROXY = ProxyCloud.parse(os.environ.get('proxy_enc','http://KIDDKDYJJKJJGDYDJKGJGJYGIHIERKDGLGDELI'))
+ACCES_USERS = os.environ.get('tl_admin_user','demian2008').split(';')
+PROXY = ProxyCloud.parse(os.environ.get('proxy_enc','http://KGGJJJYIJFLIFEYDJFLEKHYGCHDHRHHKCHHGCE'))
 
 if PROXY:
   print(f'Proxy {PROXY.as_dict_proxy()}')
