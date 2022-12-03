98b44b25f430b1540d969fb10030b5b4b6875135
Jurasio54 <103755947+Jurasio54@users.noreply.github.com>
2022-12-03 18:19:25 -0500

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ API_HASH = os.environ.get('api_hash','6817c82fe0b762ed91999f49cd17f7d7')
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
 ACCES_USERS = os.environ.get('tl_admin_user','demian2008').split(';')
-PROXY = ProxyCloud.parse(os.environ.get('proxy_enc','http://KKDDKFYDJHJEGFYDJDGIGDYDJEIJGERJCECDCDDE'))
+PROXY = ProxyCloud.parse(os.environ.get('proxy_enc','http://KFGKJDYGJKLHFIYEKGIGCGYJKDEFLJRGDKLJDJLI'))
 
 if PROXY:
   print(f'Proxy {PROXY.as_dict_proxy()}')
